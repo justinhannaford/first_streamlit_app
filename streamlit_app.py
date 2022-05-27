a592fecf3ec0edf00d91175cae6779907227d75d
Justin Hannaford <106379262+justinhannaford@users.noreply.github.com>
2022-05-27 14:57:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,8 @@ import streamlit
 
 streamlit.title('Hey, how is it going?')
 
-streamlit.header('All the thingsyou need to know')
-streamlit.text('Number 1')
-streamlit.text('Number 2')
-streamlit.text('Number 3')
-streamlit.text('Number 4')
-streamlit.text('Number 5')
+streamlit.header(🥣 🥗 🐔 🥑🍞 'All the thingsyou need to know')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
