d1e4f929101b77063b99dcfda39ef44b3bb3388d
Justin Hannaford <106379262+justinhannaford@users.noreply.github.com>
2022-05-27 14:29:32 +0100

    Update streamlit_app.py
    update text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('Hey, how is it going?')
+
+streamlit.header('All the thingsyou need to know')
+streamlit.text('Number 1')
+streamlit.text('Number 2')
+streamlit.text('Number 3')
+streamlit.text('Number 4')
+streamlit.text('Number 5')
